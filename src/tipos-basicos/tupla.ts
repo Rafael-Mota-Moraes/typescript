bfc1193f573f7f294da5b6f485c577b11b9c3142
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2023-06-18 18:09:23 -0300

    Tipo tupla

---
+++ src/tipos-basicos/tupla.ts
@@ -0,0 +1,11 @@
+const dadosCliente: [number, string] = [1, "Rafael"];
+const dadosCliente2: [number, string, string] = [1, "Rafael", "Moreira"];
+const dadosCliente3: [number, string, string, ...string[]] = [
+  1,
+  "Rafael",
+  "Moreira",
+  "Porto Alegre",
+  "RS"
+];
+
+console.log(dadosCliente);
