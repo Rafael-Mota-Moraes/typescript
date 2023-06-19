58b9d8dd1096b7e9758fea9913c7928b5475a391
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2023-06-19 11:45:42 -0300

    Union Types

---
+++ src/tipos-basicos/unionTypes.ts
@@ -0,0 +1,11 @@
+function addOrConcat(a: number | string, b: number | string) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  }
+
+  return `${a} ${b}`;
+}
+
+console.log(addOrConcat(1, 1));
+console.log(addOrConcat(1, "1"));
+console.log(addOrConcat("Rafael", "Moreira"));
