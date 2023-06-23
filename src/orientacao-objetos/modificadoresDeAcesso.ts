247f6a8609024b0c069a490b9f76f4cc5729af8f
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2023-06-23 13:41:47 -0300

    Modificadores de acesso public e private

---
+++ src/orientacao-objetos/modificadoresDeAcesso.ts
@@ -0,0 +1,42 @@
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+
+  getNome(): string {
+    return this.nome;
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string
+  ) {}
+}
+
+const empresa1 = new Empresa("Google", "100.100.100.100");
+const c1 = new Colaborador("Rafael", "Moreira");
+const c2 = new Colaborador("João", "Silva");
+
+empresa1.adicionaColaborador(c1);
+empresa1.adicionaColaborador(c2);
+
+empresa1.mostrarColaboradores();
+
+console.log(empresa1.getNome());
